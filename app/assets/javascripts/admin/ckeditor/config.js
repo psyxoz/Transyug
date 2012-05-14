e81a75944a250dfe8e1ba9428675a9c566e32727
Sergey Homenko <psyxoz@list.ru>
2012-05-14 18:49:17 +0300

    Add html editor

--- app/assets/javascripts/admin/ckeditor/config.js
+++ app/assets/javascripts/admin/ckeditor/config.js
@@ -5,7 +5,15 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.toolbar = 'Full';
+  config.toolbar_Full =
+  [
+  	{ name: 'document', items : [ 'Source','-','NewPage'] },
+  	{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+  	'/',
+  	{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+  	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+  	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+  	{ name: 'insert', items : [ 'Image','Flash','Table','SpecialChar' ] }
+  ];
 };

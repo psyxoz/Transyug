7ddcb4c555e8053e5be063750df033bf9cf28946
Sergey Homenko <psyxoz@list.ru>
2012-05-15 09:07:31 +0300

    Enable html editor

--- app/assets/javascripts/admin/ckeditor/config.js
+++ app/assets/javascripts/admin/ckeditor/config.js
@@ -5,15 +5,23 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-	config.toolbar = 'Full';
+	config.toolbar = 'Basic';
+  config.toolbar_Basic =
+  [
+    { name: 'document', items : [ 'Source','-','NewPage'] },
+    { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+    { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+    { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+    { name: 'links', items : [ 'Link','Unlink' ] }
+  ];
+
   config.toolbar_Full =
   [
   	{ name: 'document', items : [ 'Source','-','NewPage'] },
   	{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-  	'/',
   	{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
   	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
-  	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-  	{ name: 'insert', items : [ 'Image','Flash','Table','SpecialChar' ] }
+  	{ name: 'links', items : [ 'Link','Unlink' ] },
+  	{ name: 'insert', items : [ 'Table' ] }
   ];
 };

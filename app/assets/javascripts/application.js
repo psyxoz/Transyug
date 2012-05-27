81ad6941e4f52305afd049ada8850b0a4c928863
Sergey Homenko <psyxoz@list.ru>
2012-05-27 22:54:01 +0300

    modify form validators

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+
+function limitText(limitField, limitCount, limitNum) {
+  if (limitField.value.length > limitNum) {
+    limitField.value = limitField.value.substring(0, limitNum);
+  } else {
+    limitCount.value = limitNum - limitField.value.length;
+  }
+}

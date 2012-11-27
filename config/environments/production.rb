e53bfd5db6a8386806f34a9b2af3526dbae525f1
Sergey Homenko <shomenko@heliostech.hk>
2012-11-27 21:35:58 +0200

    setup production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,9 @@ Transyug::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( admin.css admin.js jquery.fancybox.css jquery.fancybox.js easySlider1.7.js jquery.ui.js
+                                  jquery.validationEngine.js jquery.validationEngine-en.js jquery.validationEngine-ru.js
+                                  validationEngine.jquery.css jquery.dropdown.js pages.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
